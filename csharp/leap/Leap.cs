e6bbfb9903796cfcbc344f88931bdbf2c7d6fa9e
tokumago <marco15@ciencias.unam.mx>
2021-06-11 19:11:17 -0700

    ADD leap solution

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -4,6 +4,21 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this function.");
+       return IsModule(year) && (!IsDiv100(year) || IsDiv400(year));
+    }
+
+    public static bool IsModule(int year)
+    {
+        return year % 4 == 0;
+    }
+
+    public static bool IsDiv100(int year)
+    {
+        return year % 100 == 0;
+    }
+
+    public static bool IsDiv400(int year)
+    {
+        return year % 400 == 0;
     }
 }
