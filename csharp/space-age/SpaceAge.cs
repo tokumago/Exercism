527e6ba77a8e89b28af21c96aaa8f631fddb0038
tokumago <marco15@ciencias.unam.mx>
2021-06-12 11:57:11 -0500

    Add Space Age solution

---
+++ csharp/space-age/SpaceAge.cs
@@ -0,0 +1,50 @@
+using System;
+
+public class SpaceAge
+{
+    public int seconds;
+    public SpaceAge(int seconds)
+    {
+        this.seconds = seconds;
+    }
+
+    public double OnEarth()
+    {
+        return (double)seconds/(double)31557600;
+    }
+
+    public double OnMercury()
+    {
+        return OnEarth() / 0.2408467;
+    }
+
+    public double OnVenus()
+    {
+        return OnEarth() / 0.61519726;
+    }
+
+    public double OnMars()
+    {
+        return OnEarth() / 1.8808158;
+    }
+
+    public double OnJupiter()
+    {
+        return OnEarth() / 11.862615;
+    }
+
+    public double OnSaturn()
+    {
+        return OnEarth() / 29.447498;
+    }
+
+    public double OnUranus()
+    {
+        return OnEarth() / 84.016846;
+    }
+
+    public double OnNeptune()
+    {
+        return OnEarth() / 164.79132;
+    }
+}
