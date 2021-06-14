5814c9e29bc5581fdf300e98e6979ed390791687
tokumago <marco15@ciencias.unam.mx>
2021-06-13 22:15:56 -0500

    ADD Nucleotide count solution

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -3,8 +3,28 @@ using System.Collections.Generic;
 
 public static class NucleotideCount
 {
+    // Given a single stranded DNA string, compute how many times each nucleotide occurs in the string.
     public static IDictionary<char, int> Count(string sequence)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        Dictionary<char, int> nucleotide = new Dictionary<char, int>() 
+        { 
+            {'A', 0},
+            {'C', 0},
+            {'G', 0},
+            {'T', 0},
+        };
+
+        foreach (char c in sequence)
+        {
+            try
+            {
+                 nucleotide[c] = ++nucleotide[c];
+            }
+            catch
+            {
+                throw new ArgumentException("Unewxpected nucleotide value");
+            }
+        }
+        return nucleotide;
     }
 }
