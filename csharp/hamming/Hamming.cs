1f7df04a63c6cc97f80fb4b91eff1d37a1ca835a
tokumago <marco15@ciencias.unam.mx>
2021-06-13 21:50:51 -0500

    Add Hamming solution

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -2,8 +2,27 @@ using System;
 
 public static class Hamming
 {
+    // Calculate the Hamming Distance between two DNA strands.
     public static int Distance(string firstStrand, string secondStrand)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (firstStrand.Length == secondStrand.Length)
+        {
+            int ham = 0;
+            for (int i = 0; i < secondStrand.Length; i++)
+            {
+                if (!secondStrand[i].Equals(firstStrand[i])) ham++;
+            }
+            return ham;
+        } 
+        else
+        {
+            throw new ArgumentException();
+        }
     }
+
+    //public static int Distance(string firstStrand, string secondStrand) {
+    //    if (firstStrand.Length != secondStrand.Length) throw new ArgumentException("YOU STUPID!");
+    //    return firstStrand.Zip(secondStrand, (s1, s2) => s1 == s2 ? 0 : 1).Sum();
+    //}
+
 }
